98c15a36438ea46d33364f2e28f7ced6e33b70b3
adnanhd <adnanharundogan@gmail.com>
2020-07-25 14:54:30 +0300

    update CoreNLP to annotate all articles under data/

--- src/main/java/CoreNLP.java
+++ src/main/java/CoreNLP.java
@@ -1,12 +1,15 @@
 
 // Import the File class
-import java.io.File;
-
 // Import this class to handle errors
-import java.io.FileNotFoundException;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 // Import the Scanner class to read text files
-import java.util.Scanner;
+import java.util.*;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+import java.util.regex.Pattern;
 
 import edu.stanford.nlp.ling.CoreLabel;
 import edu.stanford.nlp.pipeline.CoreDocument;
@@ -15,35 +18,41 @@ import edu.stanford.nlp.ling.CoreAnnotations.NamedEntityTagAnnotation;
 
 public class CoreNLP {
     public static void main(String[] args) {
-        try {
-            // Creating an object of the file for reading the data
-            File file = new File("data/10-1055-s-0040-1713678.txt");
-            Scanner scanner = new Scanner(file);
+        int count = 0;
+        File file = new File("data/");
+        File[] files = file.listFiles();
 
-            StanfordCoreNLP stanfordCoreNLP = Pipeline.getPipeline();
+        while (count < files.length)
+            try {
+                BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(
+                        new FileOutputStream("ner/" + files[0].getName()), StandardCharsets.UTF_8));
+                BufferedReader br = new BufferedReader(new InputStreamReader(
+                        new FileInputStream("data/" + files[0].getName()), StandardCharsets.UTF_8));
 
-            String text = "";
+                String line = br.readLine();
 
-            while (scanner.hasNextLine()) {
-                String data = scanner.nextLine();
-                text = text + data;
+                while (line != null) {
+                    CoreDocument coreDocument = new CoreDocument(line);
+                    StanfordCoreNLP stanfordCoreNLP = Pipeline.getPipeline();
+                    
+                    stanfordCoreNLP.annotate(coreDocument);
+
+                    List<CoreLabel> coreLabels = coreDocument.tokens();
+
+                    for (CoreLabel coreLabel : coreLabels) {
+                        String ner = coreLabel.get(NamedEntityTagAnnotation.class);
+                        bw.write(coreLabel.originalText() + " == " + ner);
+                    }
+                    line = br.readLine();
+                }
+
+                bw.close();
+                br.close();
+
+            } catch (FileNotFoundException e) {
+                System.err.println("File " + args[count] + "not found...");
+            } catch (IOException e) {
+                System.err.println("IOException in " + args[count] + "occured...");
             }
-
-            CoreDocument coreDocument = new CoreDocument(text);
-
-            stanfordCoreNLP.annotate(coreDocument);
-
-            List<CoreLabel> coreLabels = coreDocument.tokens();
-
-            for (CoreLabel coreLabel : coreLabels) {
-                String ner = coreLabel.get(NamedEntityTagAnnotation.class);
-                System.out.println(coreLabel.originalText() + " == " + ner);
-            }
-
-        } catch (FileNotFoundException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
-        }
-
     }
 }
